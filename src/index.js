dabd5923aad5f2ade614a5afb212a4bb8f592881
Franco Di Leo <dileo.francoj@gmail.com>
2023-12-30 16:46:38 -0300

    un commit IND3SC3NT3

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const port = 8005;
+const host = '0.0.0.0';
+
+app.listen(port, host, () => {
+  // eslint-disable-next-line no-console
+  console.info(`Servidor iniciado en ${port} `);
+});
